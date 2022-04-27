267c58b203e37ed4d27a557ee06868293535a29d
idle-Calliope <ellaine.fontB@gmail.com>
2022-04-27 21:39:01 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+        Template for randomizing a list of things - whether names, food, drinks, etc.
+
+        Author: ELLAINE FONTAMILLAS
+        Created: 2021, November 24
+
+ */
+
+namespace RandomizerApp
+{
+    public partial class MainInterface : Form
+    {
+        RandomizerClass rc = new RandomizerClass();
+        public MainInterface()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnMeld_Click(object sender, EventArgs e)
+        {
+            string wrd = rc.randomizer();
+            this.LblText.Text = wrd;
+            //MessageBox.Show("Meld Successful");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ RandomizerClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomizerApp
+{
+    internal class RandomizerClass
+    {
+        Random rd = new Random();
+        string[] wrds = { "Pizza", "Lasagna", "Noodles","Burger","Sandwhich"};
+        
+        public string randomizer()
+        {
+            return wrds[rd.Next(0,wrds.Length)];
+        }
+    }
+}
